cc1b8b88763a0569e4db85ac6340f8462505c1ba
unknown <amayaborrayol@gmail.com>
2024-03-17 11:49:10 -0600

    Se creo un array de canciones de luis miguel

---
+++ src/ejemploarray/EjemploArray.java
@@ -0,0 +1,18 @@
+
+package ejemploarray;
+
+
+public class EjemploArray {
+
+   
+    public static void main(String[] args) {
+      String[] cancionesLuisMiguel = {"La incondicional","La Bikina","Entregate","El reloj","Culpable o No"
+      
+    };
+        for(int i = 0; i < cancionesLuisMiguel.length; i++){
+    
+        System.out.println(cancionesLuisMiguel[i]);
+    }
+    
+}
+}
